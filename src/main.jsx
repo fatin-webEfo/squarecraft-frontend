c7d0218c24ea1ef72b298e9d20fd26168452afd3
fatin-webEfo <fatin@webefo.com>
2025-01-14 15:00:10 +0600

    react tailwind axios basic setup

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+
+const routes = [
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/about',
+    element: <div>About Page</div>,
+  },
+];
+
+const router = createBrowserRouter(routes);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
