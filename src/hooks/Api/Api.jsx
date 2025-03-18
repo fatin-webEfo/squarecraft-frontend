0de257812707d5711f5fbdad73f66521bf183972
fatin-webEfo <fatin@webefo.com>
2025-03-18 11:55:59 +0600

    admin.squareplugin.com

--- src/hooks/Api/Api.jsx
+++ src/hooks/Api/Api.jsx
@@ -1,6 +1,7 @@
-export const API = "http://62.72.7.171:8001";
+export const API = "admin.squareplugin.com";
 
 
 // https://webefo-backend.onrender.com
 // http://localhost:8000
 // http://62.72.7.171:8001
+// admin.squareplugin.com
